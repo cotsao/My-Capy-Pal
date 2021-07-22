75d278f289215ab855e1e3eb8ebae98ca0d353b2
cotsao <codytsao@gmail.com>
2021-07-22 14:57:47 -0700

    minor bug fixes

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ class CapyPal{
     hungerDrain()
     {   
         setInterval(() =>{ //drains 0-5 hunger every second
-            this.hungerLevel -= Math.floor(Math.random()*100)
+            this.hungerLevel -= Math.floor(Math.random()*5)
             
         }, 1000)
     }
